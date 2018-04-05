5c8b5acfb025fc7828b04e694fbf6a19e5439dc5
David Lilue <dvdalilue@gmail.com>
2018-04-05 16:14:24 +0200

    New data type operator constructors

--- src/Structs/Essentials/Prop.hs
+++ src/Structs/Essentials/Prop.hs
@@ -14,12 +14,17 @@
 
 module Structs.Essentials.Prop where
 
+infixl 3 :&:
+infixl 2 :|:
+infixr 1 :>:
+infix  0 :=:
+
 -- | Recursive type of logical propositions with polymorphic statements
 data Prop a
-    = And (Prop a) (Prop a) -- ^ Logical conjunction operator ('And')
-    | Or (Prop a) (Prop a) -- ^ Logical discjuntion operator ('Or')
-    | Then (Prop a) (Prop a) -- ^ Logical implecation operator. Representing "if p 'Then' q" without "if"
-    | Eq (Prop a) (Prop a) -- ^ Logical equivalence operator ('Eq')
+    = (Prop a) :&: (Prop a) -- ^ Logical conjunction operator (':&:')
+    | (Prop a) :|: (Prop a) -- ^ Logical discjuntion operator (':|:')
+    | (Prop a) :>: (Prop a) -- ^ Logical implecation operator. Representing "if p ':>:' q" without "if"
+    | (Prop a) :=: (Prop a) -- ^ Logical equivalence operator (':=:')
     | Negation (Prop a) -- ^ Logical negation unary operator ('Negation')
     | Statement a -- ^ Terminal 'Statement' of a logical expression
     deriving (Eq,Ord)
@@ -29,11 +34,11 @@ data Prop a
 instance Show a => Show (Prop a) where
     show p = go p $ -1
         where
-            go (Eq lhs rhs) pre   = shouldParent pre 0 $ (go lhs 0) ++ " ↔ " ++ (go rhs 0)
-            go (Then lhs rhs) pre = shouldParent pre 1 $ (go lhs 1) ++ " → " ++ (go rhs 1)
-            go (Or lhs rhs) pre   = shouldParent pre 2 $ (go lhs 2) ++ " ∨ " ++ (go rhs 2)
-            go (And lhs rhs) pre  = shouldParent pre 3 $ (go lhs 3) ++ " ∧ " ++ (go rhs 3)
-            go (Negation  p) pre  = "¬" ++ (go p 4)
-            go (Statement p) pre  = show p
+            go (lhs :=: rhs) pre = shouldParent pre 0 $ (go lhs 0) ++ " ↔ " ++ (go rhs 0)
+            go (lhs :>: rhs) pre = shouldParent pre 1 $ (go lhs 1) ++ " → " ++ (go rhs 1)
+            go (lhs :|: rhs) pre = shouldParent pre 2 $ (go lhs 2) ++ " ∨ " ++ (go rhs 2)
+            go (lhs :&: rhs) pre = shouldParent pre 3 $ (go lhs 3) ++ " ∧ " ++ (go rhs 3)
+            go (Negation  p) pre = "¬" ++ (go p 4)
+            go (Statement p) pre = show p
 
             shouldParent a b s = if a >= b then "(" ++ s ++ ")" else s
