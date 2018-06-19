327842b2715d6e9c3190191e2c1f9b710fc04b83
David <dvdalilue@gmail.com>
2018-06-19 20:52:23 +0200

    Memory leaks and free of freed addresses fixed

--- opencl/sat/cnf.c
+++ opencl/sat/cnf.c
@@ -12,10 +12,14 @@ Proposition* distr_cnf (Proposition *p, Proposition *q) {
     Proposition *result = NULL;
     
     if (p->kind == 2 && op(p) == AND) {
-        result = new_bin(AND, distr_cnf(lhs(p), q), distr_cnf(rhs(p), q));
+        result = new_bin(AND,
+                    distr_cnf(lhs(p), copy_prop(q)),
+                    distr_cnf(rhs(p), q));
         free_bin(p);
     } else if (q->kind == 2 && op(q) == AND) {
-        result = new_bin(AND, distr_cnf(p, lhs(q)), distr_cnf(p, rhs(q)));
+        result = new_bin(AND,
+                    distr_cnf(copy_prop(p), lhs(q)),
+                    distr_cnf(p, rhs(q)));
         free_bin(q);
     } else {
         result = new_bin(OR, p, q);
@@ -23,17 +27,20 @@ Proposition* distr_cnf (Proposition *p, Proposition *q) {
     return result;
 }
 
-Proposition* cnf (Proposition *p) {
-    Proposition *result;
-    p = nnf(p);
-    result = p;
+Proposition* cnf_aux (Proposition *p) {
+    Proposition *result = p;
+
     if (p->kind == 2) {
         if (op(p) == AND) {
-            result = new_bin(AND, cnf(lhs(p)), cnf(rhs(p)));
+            result = new_bin(AND, cnf_aux(lhs(p)), cnf_aux(rhs(p)));
         } else {
-            result = distr_cnf(cnf(lhs(p)), cnf(rhs(p)));
+            result = distr_cnf(cnf_aux(lhs(p)), cnf_aux(rhs(p)));
         }
         free_bin(p);
     }
     return result;
+}
+
+Proposition* cnf (Proposition *p) {
+    return cnf_aux(nnf(p));
 }
