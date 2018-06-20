7fc6fca706c3a8d5dab7aa4caddfbfddfb4ddb47
David <dvdalilue@gmail.com>
2018-06-20 22:50:31 +0200

    ANF rpn type for iterative version with a stack

--- opencl/sat/anf.h
+++ opencl/sat/anf.h
@@ -5,11 +5,34 @@
 //  Created by David Lilue on 16/06/2018.
 //
 
-#ifndef ANF
+#ifndef ANF_RPN
 
 #include "anf_lib.h"
 
+#define opr(rpn) rpn->anf->opr->operation
+
+typedef enum {OPN, OPR} RPN_TYPE;
+
+typedef struct _anf_rpn_opn {
+    ANF *anf;
+} ANF_RPN_OPN;
+
+typedef struct _anf_rpn_opr {
+    ANF *(*operation)(ANF*, ANF*);
+} ANF_RPN_OPR;
+
+typedef union _anf_rpn_union {
+    ANF_RPN_OPN *opn;
+    ANF_RPN_OPR *opr;
+} ANF_RPN_UNION;
+
+typedef struct _anf_rpn {
+    RPN_TYPE kind;
+    ANF_RPN_UNION *anf;
+} ANF_RPN;
+
 ANF* anf (Proposition *p);
+ANF* anf_it (Proposition *p);
 void destroy_anf (ANF *p);
 
 #endif /* anf */
