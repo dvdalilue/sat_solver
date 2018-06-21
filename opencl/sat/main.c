cfeb5ca4fc500809394c13f5ae04e6f082656176
David <dvdalilue@gmail.com>
2018-06-21 17:10:49 +0200

    Test cases

--- opencl/sat/main.c
+++ opencl/sat/main.c
@@ -10,23 +10,55 @@
 #include "anf.h"
 
 int main (int argc, const char * argv[]) {
-    Proposition *p = STM('P');
     Proposition *q = STM('Q');
-    Proposition *modus_ponen = new_bin(IMPLIE,
-                                    new_bin(AND,
-                                        new_bin(IMPLIE,
-                                            p,
-                                            q),
-                                        p),
-                                    q);
-    ANF *aux = anf(modus_ponen);
-    prop_to_s(modus_ponen);
-    fprintf(stdout, "\n");
-    
+    Proposition *p = new_bin(OR, STM('P'), q);
+    Proposition *test_1 = new_bin(IMPLIE,
+                            new_bin(AND,
+                                new_bin(IMPLIE,
+                                    p,
+                                    new_neg(q)),
+                                p),
+                            new_neg(q));
+
+    // Proposition *modus_ponen = new_bin(IMPLIE,
+    //                             new_bin(AND,
+    //                                 new_bin(IMPLIE,
+    //                                     STM('P'),
+    //                                     STM('Q'),
+    //                                 STM('P')),
+    //                             STM('Q'));
+
+    ANF *aux = anf_it(test_1);
+
+    // prop_to_s(modus_ponen);
+    // fprintf(stdout, "\n");
     anf_to_s(aux);
     fprintf(stdout, "\n");
-    destroy_prop(modus_ponen);
+
+    destroy_prop(test_1);
     destroy_anf(aux);
-    
+
     return 0;
+
+    // ANF *p = VAR('P');
+    // ANF *q = VAR('Q');
+    // ANF *aux =
+    //    and_anf(
+    //        xor_anf(
+    //            xor_anf(
+    //                xor_anf(
+    //                    and_anf(new_const(1), VAR('Q')),
+    //                    and_anf(new_const(1), p)),
+    //                xor_anf(
+    //                    and_anf(VAR('P'), new_const(1)),
+    //                    and_anf(new_const(1), q))),
+    //            xor_anf(
+    //                and_anf(new_const(1), new_const(1)),
+    //                and_anf(new_const(1),
+    //                    xor_anf(
+    //                        and_anf(p, new_const(1)),
+    //                        and_anf(new_const(1), q))))),
+    //        q);
+
+    // return 0;
 }
