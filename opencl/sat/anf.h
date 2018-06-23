0efc08817f9e1410a4ecf9d7c34b9b402dd6993e
David Lilue <dvdalilue@gmail.com>
2018-06-23 06:42:20 +0200

    Code refactoring: Iterative version of ANF transformation with RPN representation

--- opencl/sat/anf.h
+++ opencl/sat/anf.h
@@ -5,34 +5,8 @@
 //  Created by David Lilue on 16/06/2018.
 //
 
-#ifndef ANF_RPN
-
-#include "anf_lib.h"
-
-#define opr(rpn) rpn->anf->opr->operation
-
-typedef enum {OPN, OPR} RPN_TYPE;
-
-typedef struct _anf_rpn_opn {
-    ANF *anf;
-} ANF_RPN_OPN;
-
-typedef struct _anf_rpn_opr {
-    ANF *(*operation)(ANF*, ANF*);
-} ANF_RPN_OPR;
-
-typedef union _anf_rpn_union {
-    ANF_RPN_OPN *opn;
-    ANF_RPN_OPR *opr;
-} ANF_RPN_UNION;
-
-typedef struct _anf_rpn {
-    RPN_TYPE kind;
-    ANF_RPN_UNION *anf;
-} ANF_RPN;
+#include "anf_rpn.h"
 
 ANF* anf (Proposition *p);
 ANF* anf_it (Proposition *p);
 void destroy_anf (ANF *p);
-
-#endif /* anf */
