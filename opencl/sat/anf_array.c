900e1c7d7191be24337063100e365b841409c9de
David <dvdalilue@gmail.com>
2018-06-24 17:14:05 +0200

    New ANF representation with dynamic arrays

---
+++ opencl/sat/anf_array.c
@@ -0,0 +1,105 @@
+//
+//  anf_array.c
+//  sat
+//
+//  Created by David Lilue on 23/06/2018.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "anf_array.h"
+
+void array_init(Array *a) {
+    for (int i = 0; i < a->size; i++) {
+        index(a, i, void *) = NULL;
+    }
+}
+
+ANF_Array* new_anf_array (void) {
+    ANF_Array *anf_a = (ANF_Array *) malloc(sizeof(ANF_Array));
+    ANF_BitString *bs = (ANF_BitString *) malloc(sizeof(ANF_BitString));
+    array_new(&bs->bstring, 128, char);
+    
+    for (int i = 0; i < 128; i++) {
+        index(bs->bstring, i, char) = 0;
+    }
+    
+    array_new(&anf_a->xors, 7, ANF_BitString*);
+    
+    index(anf_a->xors, 0, ANF_BitString*) = bs;
+    anf_a->components++;
+    
+    return anf_a;
+}
+
+ANF_Array* one (void) {
+    return new_anf_array();
+}
+
+ANF_Array* zero (void) {
+    ANF_Array *z = new_anf_array();
+    
+    for (int i = 0; i < 128; i++) {
+        get_bs_comp(z, 0, i) = -1;
+    }
+
+    return z;
+}
+
+ANF_Array* new_poly (int var) {
+    ANF_Array *p = new_anf_array();
+    
+    get_bs_comp(p, 0, var) = 1;
+    get_anf_bs(p, 0)->bits_on++;
+    
+    return p;
+}
+
+//ANF_Array* xor_anf_array (ANF_Array *p, ANF_Array *q) {
+//    
+//}
+
+Ordering compare_bs (ANF_BitString *x, ANF_BitString *y) {
+    if (x->bits_on < y->bits_on) {
+        return LT;
+    } else if (x->bits_on > y->bits_on) {
+        return GT;
+    } else {
+        if (index(x->bstring, 0, char) == -1  &&
+            index(y->bstring, 0, char) == -1) { return EQ; }
+        if (index(x->bstring, 0, char) == -1) { return LT; }
+        if (index(y->bstring, 0, char) == -1) { return GT; }
+        if (x->bits_on == 0) { return EQ; }
+
+        for (int i = 0; i < x->bstring->size; i++) {
+            if (index(x->bstring, i, char) == 1 &&
+                index(y->bstring, i, char) == 0)
+            {
+                return LT;
+            } else if (index(x->bstring, i, char) == 0 &&
+                       index(y->bstring, i, char) == 1)
+            {
+                return GT;
+            }
+        }
+    }
+
+    return EQ;
+}
+
+void free_array(Array *a) {
+    free(a->elements);
+    free(a);
+}
+
+void free_anf_array (ANF_Array *p) {
+    for (int i = 0; i < p->xors->size; i++) {
+        if (index(p->xors, i, ANF_BitString*) != NULL) {
+            free_array(index(p->xors, i, ANF_BitString*)->bstring);
+            free(index(p->xors, i, ANF_BitString*));
+        }
+    }
+    
+    free_array(p->xors);
+    free(p);
+}
---
+++ opencl/sat/anf_array.h
@@ -0,0 +1,59 @@
+//
+//  anf_array.h
+//  sat
+//
+//  Created by David Lilue on 23/06/2018.
+//
+
+#ifndef Array
+
+#define index(a, i, t) ((t*) a->elements)[i]
+#define array_new(a, size_t, type) ({ \
+            *a = (Array *) malloc(sizeof(Array)); \
+            (*a)->size = size_t; \
+            (*a)->elements = (void *) malloc(sizeof(type) * size_t); \
+            array_init(*a); \
+        })
+#define get_anf_bs(anf, i) index(anf->xors, i, ANF_BitString *)
+#define get_bs(anf, i) get_anf_bs(anf, i)->bstring
+#define get_bs_comp(anf, i, j) index(get_bs(anf, i), j, char)
+#define min(a, b) a < b ? a : b
+
+typedef enum {LT, GT, EQ} Ordering;
+
+typedef struct _array {
+    int size;
+    void *elements;
+} Array;
+
+typedef struct _anf_bit_string {
+    int bits_on;
+    Array *bstring;
+} ANF_BitString;
+
+typedef struct _anf_xor_array {
+    int components;
+    Array *xors;
+} ANF_Array;
+
+void array_init (Array *ar);
+ANF_Array* one (void);
+ANF_Array* zero (void);
+ANF_Array* new_poly (int var);
+ANF_Array* xor_anf_array (ANF_Array *p, ANF_Array *q);
+
+Ordering compare_bs (ANF_BitString *x, ANF_BitString *y);
+
+////ANF_Array* new_bin_anf (OpANF op, ANF_Array *p, ANF_Array *q);
+//ANF_Array* and_anf (ANF_Array *p, ANF_Array *q);
+//ANF_Array* distr_anf (ANF_Array *p, ANF_Array *q);
+//ANF_Array* or_to_xor (ANF_Array *lhs, ANF_Array *rhs);
+//ANF_Array* implie_to_xor (ANF_Array *lhs, ANF_Array *rhs);
+//ANF_Array* eq_to_xor (ANF_Array *lhs, ANF_Array *rhs);
+////ANF_Array* reduce (ANF_Array *p);
+////ANF_Array* copy (ANF_Array *p);
+
+void free_anf_array (ANF_Array *p);
+//void anf_to_s (ANF_Array *p);
+
+#endif /* array_h */
