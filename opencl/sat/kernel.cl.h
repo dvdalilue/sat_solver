11ceed8406fc8b33bc393bc0d495f54c106650e9
David Lilue <dvdalilue@gmail.com>
2018-07-15 15:04:17 +0200

    Older ANF representations

--- opencl/sat/kernel.cl.h
+++
@@ -1,6 +0,0 @@
-/***** GCL Generated File *********************/
-/* Automatically generated file, do not edit! */
-/**********************************************/
-
-#include <OpenCL/opencl.h>
-extern void (^and_bs_gpu_kernel)(const cl_ndrange *ndrange, cl_char* bs_x, cl_char* bs_y, cl_char* bs_out);
